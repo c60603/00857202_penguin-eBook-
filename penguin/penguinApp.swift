4f919cf726b3b2177dfe9cab30afede58be5fba2
c8610290927 <c8610290927@gmail.com>
2020-10-12 16:24:46 +0800

    Initial Commit

---
+++ penguin/penguinApp.swift
@@ -0,0 +1,20 @@
+//
+//  penguinApp.swift
+//  penguin
+//
+//  Created by User20 on 2020/10/12.
+//
+
+import SwiftUI
+
+@main
+struct penguinApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
